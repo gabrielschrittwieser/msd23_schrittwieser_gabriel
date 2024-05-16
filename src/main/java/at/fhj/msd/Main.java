010c56269fe1921d77b4769d9edcd2699d6e8de3
Gabriel Schrittwieser <gabriel.schrittwieser@edu.fh-joanneum.at>
2024-05-16 09:41:13 +0200

    added Log4j Loggers

--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,9 +1,14 @@
 package at.fhj.msd;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
     public static void main(String[] args) {
+        Logger logger = LogManager.getLogger();
         Calculator.calculator();
         Calculator.scanner.close();
+        logger.info("This is a info logger.");
         System.out.println("\nGabriel Schrittwieser");
+        logger.error("This is an error logger.");
     }
 }
